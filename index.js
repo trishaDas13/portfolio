ccf3bd5088706417fd0bbe28a1f07ca00d325184
trishaDas13 <trisha.das1308@gmail.com>
2023-11-28 15:08:06 +0530

    responsiveness of home page done

--- index.js
+++ index.js
@@ -65,4 +65,15 @@ let typed = new Typed(".auto-type",{
   loop: true,
 });
 
+//todo -------- responsive navbar design --------
+let bar = document.querySelector('.bar');
+let dropdown = document.querySelector('.responsive-navbar');
 
+bar.addEventListener('click', ()=>{
+  if(dropdown.style.display == 'none'){
+    dropdown.style.display = 'inline';
+  }
+  else{
+    dropdown.style.display = 'none';
+  }
+});
